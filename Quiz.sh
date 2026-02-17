a90d48be7275d007af8a26c95997b76c00f93c77
MaxWeber03 <maxweber@uni-bremen.de>
2026-02-17 09:44:51 +0100

    Started writing report methods

--- Quiz.sh
+++ Quiz.sh
@@ -7,6 +7,9 @@ cat 01_raw/ProjTaxa.vcf |  grep -v ^\## | less -S
 grep -v ^\# 01_raw/ProjTaxa.vcf | wc -l
 # 3816977
 
+grep -v ^\# 02_vcf_filtered/an_ac_filtered.vcf | wc -l
+# 112329
+
 # How many chromosomes are present in the vcf?
 # chromosomes start with "##contig=<ID=chr", so we can count those lines
 grep ^\#\#contig\=\<ID\=chr* 01_raw/ProjTaxa.vcf | wc -l
